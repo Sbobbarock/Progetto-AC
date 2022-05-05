82646471010f6232e1b9e696251944b0a6051ab3
Filippo Sechi <104740983+Sbobbarock@users.noreply.github.com>
2022-05-05 19:50:31 +0200

    Update server.c
    Ogni connessione con un client viene gestita da un thread diverso. Meccanismo di invio pacchetti--> prima mando la dimensione, poi invio il pacchetto

--- src/server.c
+++ src/server.c
@@ -7,14 +7,81 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <string.h>
+#include <pthread.h>
 
 #define BUF_SIZE 10
+#define MAX_CONNECTED 5
+
+////////////////////////////////////
+// COMPILARE CON FLAG -lpthread !//
+//////////////////////////////////
+
+//invia la dimensione del pacchetto e successivamente il pacchetto stesso
+void send_packet(int sd,char* msg){
+    int ret;
+    int dim_msg = htonl(strlen(msg)+1);
+
+    ret = send(sd,&dim_msg,sizeof(uint32_t),0);
+    if(!ret){
+            close(sd);
+            pthread_exit(NULL);
+    }
+
+    dim_msg = ntohl(dim_msg);
+    ret = send(sd,msg,dim_msg,0);
+    if(!ret){
+        close(sd);
+        pthread_exit(NULL);
+    }
+}
+
+//riceve la dimensione del pacchetto e successivamente il pacchetto stesso
+char* recv_packet(int sd){
+    int ret;
+    char * buffer;
+    int dim_msg;
+
+    ret = recv(sd,&dim_msg,sizeof(uint32_t),0);
+    if(!ret)
+        return NULL;
+
+    dim_msg = ntohl(dim_msg);
+    buffer = malloc(dim_msg);
+    if(!buffer)
+        return NULL;
+
+    ret = recv(sd,buffer,dim_msg,0);
+    if(!ret)
+        return NULL;
+    
+    return buffer;
+}
+
+// routine eseguita dal thread
+void* manageConnection(void* s){
+    int ret = 0;
+    char* buffer;
+    int soc = *((int*)s);
+    int dim_msg;
+    // il client manda sempre il primo messaggio
+    while(1){
+        buffer = recv_packet(soc);
+        if(!buffer){
+            close(soc);
+            pthread_exit(NULL);
+        }
+        printf("Ricevuto: %s",buffer);
+        printf("INVIO: %s",buffer);
+
+        send_packet(soc,buffer);
+    }
+}
 
 int main(int n_args, char** args){
     int i;
     int porta; /*porta del server */
     int listener; /* socket listener TCP */
-    int client_sd;
+    int* client_sd;
     socklen_t len;
     struct sockaddr_in server, client;
     int ret; /*controllo errori */
@@ -23,14 +90,16 @@ int main(int n_args, char** args){
 
     /* variabili multiplexing I/O */
     int max_fd;
-    fd_set MASTER;
-    fd_set READY;
+    fd_set MASTER,READY;
+
+    /* multithreading */
+    pthread_t thread_id;
 
     if(n_args != 2){
         printf("Errore: numero di porta non inserito\n");
         exit(0);
     }
-    ret = sscanf(args[1],"%d",&porta); // OVERFLOW?
+    ret = sscanf(args[1],"%d",&porta); /* OVERFLOW? */
     if(!ret){
         printf("Numero di porta non valido\n");
         exit(0);
@@ -47,7 +116,7 @@ int main(int n_args, char** args){
         perror("Errore: ");
         exit(0);
     }
-    if(listen(listener,10) == -1){
+    if(listen(listener,MAX_CONNECTED) == -1){
         perror("Errore: ");
         exit(0);
     }
@@ -60,38 +129,28 @@ int main(int n_args, char** args){
     max_fd = listener;
     while(1){
         READY = MASTER;
-        /* Bloccante. Si sblocca quando almeno uno dei socket (o stdin) e' pronto in
-            lettura o scrittura. Attenzione: in READY rimangono solo i socket pronti */
-        select(max_fd+1, &READY, NULL, NULL, NULL); 
+        select(max_fd+1, &READY, NULL, NULL, NULL);
         for(i = 0; i <= max_fd; i++){
             if(!FD_ISSET(i,&READY))
                 continue;
             else if(i == STDIN_FILENO){   
-                /*
-                fgets(buffer,BUF_SIZE,stdin); //NON SICURO: SOLO PER PROVA 
+                
+                fgets(buffer,BUF_SIZE,stdin); // NON SICURO: SOLO PER PROVA 
                 printf("Letto: %s",buffer);
-                */
+                
             }
             else if(i == listener){ /* richiesta di connessione al server */
                 int addrlen = sizeof(client);
-                client_sd = accept(listener,(struct sockaddr*)&client,&addrlen); //nuovo socket connesso con il client
-                FD_SET(client_sd, &MASTER);
-                if(client_sd > max_fd) max_fd = client_sd;
-            }
-            else{
-                /* ret = 0;
-                while(ret += recv(i,buffer,BUF_SIZE,0) < BUF_SIZE)  //controlla di averli ricevuti tutti i byte 
-                    if(!ret){
-                        close(i);
-                        FD_CLR(i,&MASTER);
-                    }
-                
-                    else{
-                        printf("Ricevuto: %s",buffer);
-                        printf("INVIO: %s",buffer);
-                        send(i,buffer,BUF_SIZE,0);
-                    }
-                 */
+                client_sd = (int *)malloc(sizeof(int));
+                if(!client_sd){
+                    printf("Errore\n");
+                    exit(0);
+                }
+                *client_sd = accept(listener,(struct sockaddr*)&client,&addrlen);
+                if(pthread_create(&thread_id,NULL,(void*)manageConnection,(void*)client_sd)){ //crea un nuovo thread che gestisce il socket client-server
+                    printf("Errore creazione del thread\n");
+                    close(*client_sd);
+                }
             }
         }
     }
