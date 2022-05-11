ce0b1e34de7c3777aef74ebcd5ac7f456165bc4e
Filippo Sechi <104740983+Sbobbarock@users.noreply.github.com>
2022-05-11 18:02:29 +0200

    Update client.c

--- src/client.c
+++ src/client.c
@@ -7,40 +7,50 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <string.h>
+#include <iostream>
 
-#define BUF_SIZE 10
+
 #define IP "127.0.0.1"
+#define MAX_USER_INPUT 10
 
 int main(int n_args, char** args){
     int porta;
     int ret;
-    int sd;
-    char buffer[BUF_SIZE];
+    int sd; //socket id
+    char user_input[MAX_USER_INPUT];
     socklen_t len;
     struct sockaddr_in server;
     fd_set MASTER, READY;
     int max_fd;
     int dim_msg;
+    char* buffer;
 
     if(n_args != 2){
-        printf("Errore: numero di porta non inserito\n");
-        exit(0);
+        std::cout<<"Numero di porta non inserito\n";
+        exit(1);
     }
-    ret = sscanf(args[1],"%d",&porta); /* OVERFLOW? */
+    ret = sscanf(args[1],"%d",&porta);
     if(!ret){
-        printf("Numero di porta non valido\n");
-        exit(0);
+        std::cout<<"Numero di porta non valido\n";;
+        exit(1);
     }
 
     sd = socket(AF_INET,SOCK_STREAM,0);
+    if(sd == -1){
+        perror("Error:");
+        exit(1);
+    }
     memset(&server,0,sizeof(server));
     server.sin_family = AF_INET;
     server.sin_port = htons(porta);
-    inet_pton(AF_INET,IP,&server.sin_addr);
+    if(inet_pton(AF_INET,IP,&server.sin_addr) != 1){
+        perror("Error:");
+        exit(1);
+    }
     ret = connect(sd,(struct sockaddr*)&server,sizeof(server));
     if(ret == -1){
-        perror("Errore:");
-        exit(0);
+        perror("Error:");
+        exit(1);
     }
     FD_ZERO(&MASTER);
     FD_ZERO(&READY);
@@ -56,35 +66,70 @@ int main(int n_args, char** args){
             if(!FD_ISSET(i,&READY))
                 continue;
             else if(i == STDIN_FILENO){
-                fgets(buffer,BUF_SIZE,stdin);
-                printf("Letto e invio: %s",buffer);
+                if(!fgets(user_input,MAX_USER_INPUT,stdin)){
+                    std::cerr<<"Error in reading user input\n";
+                    exit(1);
+                }
+                char* p = strchr(user_input,'\n');
+                if(p)
+                    *p = '\0';
+                else{
+                    scanf("%*[^\n]");
+                    scanf("%*c");
+                }
+                std::cout<<"INVIO: "<<user_input<<std::endl;
+                
                 //esempio di come inviare un intero sul network
-                dim_msg = strlen(buffer)+1;
+                dim_msg = strlen(user_input)+1;
                 dim_msg = htonl(dim_msg); //standardizza la endianess 
-                send(sd,&dim_msg,sizeof(uint32_t),0); // gestione errore se non manda tutti i byte? 
+                ret = send(sd,&dim_msg,sizeof(uint32_t),0); 
+                while(ret < sizeof(uint32_t)) //gestione errore se non invia tutti i byte
+                    ret += send(sd,&dim_msg,sizeof(uint32_t),0);
 
                 //dopo aver inviato la dimensione del pacchetto, invio il pacchetto vero e propio
                 dim_msg = ntohl(dim_msg); //ripristina la endianess usata del sistema
-                send(sd,buffer,dim_msg,0);
+                ret = send(sd,user_input,dim_msg,0);
+                while(ret < dim_msg)
+                    ret += send(sd,user_input,dim_msg,0);
             }
+
             else if(i == sd){
                 //ricevo prima la dimensione del pacchetto
                 ret = recv(i,&dim_msg,sizeof(uint32_t),0);
-                dim_msg = ntohl(dim_msg);
                 if(!ret){
                     close(i);
                     FD_CLR(i,&MASTER);
+                    std::cout<<"Server disconnected\n";
                     exit(0); 
                 }
+                while(ret < sizeof(uint32_t))
+                    ret += recv(i,&dim_msg,sizeof(uint32_t),0);
+
+                dim_msg = ntohl(dim_msg);
+                buffer = (char*) malloc(dim_msg);
+                if(!buffer){
+                    std::cerr<<"Error in allocation of buffer for received packet\n";
+                    close(i);
+                    exit(1);
+                }
+
                 ret = recv(i,buffer,dim_msg,0);
                 if(!ret){
                     close(i);
                     FD_CLR(i,&MASTER);
+                    free(buffer);
+                    std::cout<<"Server disconnected\n";
                     exit(0); 
                 }
-                printf("RICEVUTO: %s",buffer);  
+                while(ret < dim_msg)
+                    ret += recv(i,buffer,dim_msg,0);
+
+                buffer[dim_msg-1] = '\0';
+                std::cout<<"Ricevuto: "<<buffer<<std::endl; 
+                free(buffer); 
             }
         }
     }
+    free(buffer);
     close(sd);
 }
