aadc6faf48546f4ca53dfb2a82a0e2b0890d41ff
Filippo Sechi <104740983+Sbobbarock@users.noreply.github.com>
2022-05-18 10:38:31 +0200

    Update and rename packet.h to signature.cpp

---
+++ src/lib/signature.cpp
@@ -0,0 +1,30 @@
+#include "header/signature.h"
+
+unsigned char* compute_signature(const EVP_MD* Hash_type, unsigned char* msg, int msg_len, EVP_PKEY* key,uint32_t* signature_len){
+
+    unsigned char* signature = (unsigned char*)malloc(EVP_PKEY_size(key));
+    if(!signature)
+        return NULL;
+    EVP_MD_CTX* ctx = EVP_MD_CTX_new();
+    EVP_SignInit(ctx,Hash_type);
+    EVP_SignUpdate(ctx,msg,size_t(msg_len));
+    EVP_SignFinal(ctx,signature,signature_len,key);
+    EVP_MD_CTX_free(ctx);
+
+    return signature;
+}
+
+bool verify_signature(const EVP_MD* Hash_type, unsigned char* signature, int sign_len, EVP_PKEY* key,unsigned char* msg,uint32_t msg_len){
+
+    int ret;
+    EVP_MD_CTX* ctx = EVP_MD_CTX_new();
+    EVP_VerifyInit(ctx,Hash_type);
+    EVP_VerifyUpdate(ctx,msg,size_t(msg_len));
+    ret = EVP_VerifyFinal(ctx,signature,sign_len,key);
+    if(ret != 1){
+        EVP_MD_CTX_free(ctx);
+        return false;
+    }
+    EVP_MD_CTX_free(ctx);
+    return true;
+}
