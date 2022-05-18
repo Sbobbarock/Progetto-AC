c4dbf03b28749f9c9e27f906a8c74035873bd4e6
Filippo Sechi <104740983+Sbobbarock@users.noreply.github.com>
2022-05-18 10:42:47 +0200

    Update certificate.h

--- src/lib/header/certificate.h
+++ src/lib/header/certificate.h
@@ -1,1 +1,8 @@
+#include <openssl/x509.h>
+#include <iostream>
+#include <openssl/pem.h>
+#include <openssl/x509_vfy.h>
 
+X509_STORE* build_store(std::string ,std::string );
+X509* read_certificate(std::string,unsigned char* ,uint32_t);
+EVP_PKEY* validate_certificate(X509_STORE* , X509* ); 
