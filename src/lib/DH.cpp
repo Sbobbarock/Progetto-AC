cdbb539e0e5aef48676363592a617a693ba8edbd
Filippo Sechi <104740983+Sbobbarock@users.noreply.github.com>
2022-05-18 10:37:08 +0200

    Update DH.cpp

--- src/lib/DH.cpp
+++ src/lib/DH.cpp
@@ -1,4 +1,4 @@
-#include "DH.h"
+#include "header/DH.h"
 
 EVP_PKEY* DH_privkey(){
     
@@ -9,11 +9,12 @@ EVP_PKEY* DH_privkey(){
     EVP_PKEY_keygen_init(dh_ctx);
     EVP_PKEY_keygen(dh_ctx,&my_privkey);
     EVP_PKEY_CTX_free(dh_ctx);
+    EVP_PKEY_free(dh_params);
     return my_privkey;
 }
 
 //ritorna il buffer contenente il file PEM della chiave pubblica
-unsigned char* DH_pubkey(std::string filename,EVP_PKEY* my_privkey,uint32_t* file_len){
+unsigned char* DH_pubkey(std::string filename,EVP_PKEY* my_privkey,EVP_PKEY* pub_key,uint32_t* file_len){
     FILE* pubkey_PEM = fopen(filename.c_str(),"w+");
     if(!pubkey_PEM){
         std::cout<<"Errore nella creazione del file PEM per la chiave pubblica\n";
@@ -44,6 +45,9 @@ unsigned char* DH_pubkey(std::string filename,EVP_PKEY* my_privkey,uint32_t* fil
         return NULL;
     }
     
+    rewind(pubkey_PEM);
+    pub_key = PEM_read_PUBKEY(pubkey_PEM,NULL,NULL,NULL);
+
     fclose(pubkey_PEM);
     return buffer;
 }
@@ -74,19 +78,46 @@ EVP_PKEY* DH_derive_pubkey(std::string filename,unsigned char* buffer,uint32_t f
     return received_pubkey;
 }
 
-unsigned char* DH_derive_session_secret(EVP_PKEY* my_privkey, EVP_PKEY* received_pubkey,size_t* key_len){
+unsigned char* DH_derive_session_secret(EVP_PKEY* my_privkey, EVP_PKEY* received_pubkey,size_t* secret_len){
 
     EVP_PKEY_CTX* ctx_drv = EVP_PKEY_CTX_new(my_privkey,NULL);
     EVP_PKEY_derive_init(ctx_drv);
     EVP_PKEY_derive_set_peer(ctx_drv,received_pubkey);
-    unsigned char* K_ab;
-    EVP_PKEY_derive(ctx_drv,NULL,key_len);
+    unsigned char* secret;
+    EVP_PKEY_derive(ctx_drv,NULL,secret_len);
 
-    K_ab = (unsigned char*)malloc(*key_len);
-    if(!K_ab){
+    secret = (unsigned char*)malloc(*secret_len);
+    if(!secret){
+        EVP_PKEY_CTX_free(ctx_drv);
         std::cout<<"Errore nella malloc()\n";
         return NULL;
     }
-    EVP_PKEY_derive(ctx_drv,K_ab,key_len);
-    return  K_ab;
+    EVP_PKEY_derive(ctx_drv,secret,secret_len);
+    EVP_PKEY_CTX_free(ctx_drv);
+    return  secret;
+}
+
+unsigned char* session_key(const EVP_MD* Hash_type,const EVP_CIPHER* Cipher_type, unsigned char* msg,size_t msg_len,unsigned int* digest_len){
+    
+    unsigned char* full_digest = (unsigned char*)malloc(EVP_MD_size(Hash_type));
+    if(!full_digest)
+        return NULL;
+    EVP_MD_CTX* ctx = EVP_MD_CTX_new();
+    EVP_DigestInit(ctx,Hash_type);
+    EVP_DigestUpdate(ctx,msg,msg_len);
+    EVP_DigestFinal(ctx,full_digest,digest_len);
+    EVP_MD_CTX_free(ctx);
+
+    if(*digest_len > EVP_CIPHER_key_length(Cipher_type)){
+        unsigned char* digest = (unsigned char*)malloc(EVP_CIPHER_key_length(Cipher_type));
+        if(!digest){
+            free(full_digest);
+            return NULL;
+        }
+        memcpy(digest,full_digest,EVP_CIPHER_key_length(Cipher_type));
+        free(full_digest);
+        *digest_len = EVP_CIPHER_key_length(Cipher_type);
+        return digest;
+    }
+    return full_digest;
 }
