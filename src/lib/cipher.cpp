7c66faeccb922482ad7a0a04b4ca0d3f5815aab3
Filippo Sechi <104740983+Sbobbarock@users.noreply.github.com>
2022-05-24 17:10:10 +0200

    Update cipher.cpp

--- src/lib/cipher.cpp
+++ src/lib/cipher.cpp
@@ -1,7 +1,6 @@
-#include "openssl/evp.h"
 #include "header/cipher.h"
 
-int encrypt(unsigned char *plaintext, int plaintext_len,
+bool encrypt_gcm(unsigned char *plaintext, int plaintext_len,
                 unsigned char *aad, int aad_len,
                 unsigned char *key,
                 unsigned char *iv,
@@ -13,26 +12,67 @@ int encrypt(unsigned char *plaintext, int plaintext_len,
     int ciphertext_len=0;
 
     if(!(ctx = EVP_CIPHER_CTX_new()))
-        return 0;
+        return false;
 
     if(1 != EVP_EncryptInit(ctx, EVP_aes_128_gcm(), key, iv))
-        return 0;
+        return false;
 
     
     if(1 != EVP_EncryptUpdate(ctx, NULL, &len, aad, aad_len))
-        return 0;
+        return false;
 
     if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
-        return 0;
+        return false;
     ciphertext_len = len;
 
     if(1 != EVP_EncryptFinal(ctx, ciphertext + len, &len))
-        return 0;
+        return false;
     ciphertext_len += len;
 
     if(1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_AEAD_GET_TAG, 16, tag))
-        return 0;
+        return false;
 
     EVP_CIPHER_CTX_free(ctx);
-    return ciphertext_len;
+    return true;
+}
+
+bool decrypt_gcm(unsigned char *ciphertext, int ciphertext_len,
+                unsigned char *aad, int aad_len,
+                unsigned char *tag,
+                unsigned char *key,
+                unsigned char *iv,
+                unsigned char *plaintext)
+{
+    EVP_CIPHER_CTX *ctx;
+    int len;
+    int plaintext_len;
+    int ret;
+
+    if(!(ctx = EVP_CIPHER_CTX_new()))
+        return false;
+    if(!EVP_DecryptInit(ctx, EVP_aes_128_gcm(), key, iv))
+        return false;
+
+    if(!EVP_DecryptUpdate(ctx, NULL, &len, aad, aad_len))
+        return false;
+
+    if(!EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+        return false;
+    plaintext_len = len;
+
+    if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_AEAD_SET_TAG, 16, tag))
+        return false;
+
+    ret = EVP_DecryptFinal(ctx, plaintext + len, &len);
+
+    EVP_CIPHER_CTX_cleanup(ctx);
+
+    if(ret > 0) {
+        /* Success */
+        plaintext_len += len;
+        return true;
+    } else {
+        /* Verify failed */
+        return false;
+    }
 }
