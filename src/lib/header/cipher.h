a232983c1c2d3a5ef187d1b2552d1286c93a4234
Filippo Sechi <104740983+Sbobbarock@users.noreply.github.com>
2022-05-27 13:10:19 +0200

    Update cipher.h

--- src/lib/header/cipher.h
+++ src/lib/header/cipher.h
@@ -1,4 +1,4 @@
 #include <openssl/evp.h>
 
-bool encrypt_gcm(unsigned char *, int ,unsigned char *, int ,unsigned char *,unsigned char *,unsigned char *,unsigned char *);
-bool decrypt_gcm(unsigned char *, int ,unsigned char *, int ,unsigned char *,unsigned char *,unsigned char *,unsigned char *);
+bool encrypt_gcm(unsigned char *, int ,unsigned char *, int ,unsigned char *,unsigned char *,unsigned char *,uint32_t*,unsigned char *);
+bool decrypt_gcm(unsigned char *, uint32_t ,unsigned char *, int ,unsigned char *,unsigned char *,unsigned char *,unsigned char *,uint32_t*);
