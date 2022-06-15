d696766f0c8ec066356332816b78bccef11c3380
DavideBertolucci <72885704+DavideBertolucci@users.noreply.github.com>
2022-06-15 14:24:01 +0200

    Add files via upload

--- src/lib/header/utility.h
+++ src/lib/header/utility.h
@@ -6,7 +6,6 @@
 #include <openssl/rand.h>
 #include <openssl/evp.h>
 #include <openssl/pem.h>
-#include <fcntl.h>
 #include <cmath>
 #include "cipher.h"
 
@@ -23,17 +22,14 @@
 template<class T> 
 bool send_packet(int sd, T* msg, int len){
     int ret = 0;
-    int tmp = 0;
     do{
-        tmp = send(sd,msg,len,0);
-        if(tmp == -1) {
+        ret += send(sd,msg,len,0);
+        if(!ret)
             return false;
-        }
-        ret += tmp;
-        tmp = 0;
-    }
-    while(ret < len); 
+    }while(ret < len); 
+
     return true;
+    
 }
 
 template <class T>
@@ -58,12 +54,17 @@ T* recv_packet(int sd,int len){
     return buffer;
 }
 
+
+
+/*Funzione che genera un numero randomico detto NONCE */
 unsigned char* nonce(unsigned char* buffer, unsigned int nonce_len){
     RAND_poll();
     RAND_bytes(buffer,nonce_len);
     return buffer;
 }
 
+
+/*Funzione che legge il file PEM contente la chiave privata RSA */
 EVP_PKEY* read_RSA_privkey(std::string filename){
     FILE* file = fopen(filename.c_str(),"r");
     if(!file) {
@@ -74,6 +75,7 @@ EVP_PKEY* read_RSA_privkey(std::string filename){
     return key;
 }
 
+/*Funzione che legge il file PEM contente la chiave pubblica RSA */
 EVP_PKEY* read_RSA_pubkey(std::string username){
     std::string file_name;
     file_name = "PubKeyList/" + username + ".pem";
@@ -86,6 +88,7 @@ EVP_PKEY* read_RSA_pubkey(std::string username){
     return key;
 }
 
+/*Funzione che invia un file*/
 bool send_file(int sd,std::string file){
 
     FILE* f = fopen(file.c_str(),"r");
@@ -116,36 +119,21 @@ bool check_string(std::string s){
     static char ok_chars[] = "abcdefghijklmnopqrstuvwxyz"
                             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                             "1234567890."; 
-    if(s.find_first_not_of(ok_chars) != std::string::npos || (s.at(0) == '.' && s.length() == 1) || (s.at(0) == '.' && s.at(1) == '.' && s.length() == 2)){
+    if(s.find_first_not_of(ok_chars) != std::string::npos){
         std::cout<<"Stringa non valida\n";
         return false;
     }
     return true;
 }
 
-void clean_socket(int sd){
-    
-    std::cout<<"Ripristino della connessione in corso...\n";
-    fcntl(sd, F_SETFL, fcntl(sd, F_GETFL, 0) | O_NONBLOCK);
-    unsigned char* buffer = (unsigned char*)malloc(500);
-    int ret = recv(sd,buffer,500,0);
-    while(ret != -1){
-        ret = recv(sd,buffer,500,0);
-        if(ret < 500){
-            usleep(60000);
-            ret = recv(sd,buffer,500,0);
-        }
-    }
-    fcntl(sd, F_SETFL, fcntl(sd, F_GETFL, 0));
-    free(buffer);
-    std::cout<<"Ripristino completato";
-    std::cout<<std::endl;
-    return;
-}
 
+/*Funzione che costruisce la AAD di un pacchetto standard:  
+  1) counter 
+  2) id 
+  3) num_packets 
+  4) iv */
 unsigned char* build_aad_std(uint64_t counter, uint32_t num_packets, uint8_t id, unsigned char* iv){
 
-    //aad = counter || id || num_packets || iv
     unsigned char* aad = (unsigned char*)malloc(STD_AAD_LEN);
     if(!aad){
         return NULL;
@@ -158,6 +146,12 @@ unsigned char* build_aad_std(uint64_t counter, uint32_t num_packets, uint8_t id,
     return aad;
 }
 
+
+
+/*Metodo per costruire una richiesta standard: 
+  1) AAD
+  2) Ciphertext
+  3) TAG */
 unsigned char* build_request(unsigned char* aad, unsigned char* ciphertext, unsigned char* tag,uint32_t aad_len, uint32_t cipher_len){
 
     unsigned char* request = (unsigned char*)malloc(cipher_len + aad_len + 16);
@@ -173,9 +167,10 @@ unsigned char* build_request(unsigned char* aad, unsigned char* ciphertext, unsi
 }
 
 
+/*Metodo per inviare un pacchetto di richiesta standard*/
 void send_std_packet(std::string filename, unsigned char* key,int sd, uint64_t* counter, uint8_t id, uint32_t num_packets){
 
-
+    //allocco IV 
     unsigned char* iv = (unsigned char*)malloc(12);
     if(!iv){
         std::cout<<"Errore nella malloc\n";
@@ -183,12 +178,15 @@ void send_std_packet(std::string filename, unsigned char* key,int sd, uint64_t* 
     }
     iv = nonce(iv,12);
 
+    //creo AAD 
     unsigned char* aad = build_aad_std(*counter,num_packets,id,iv);
     if(!aad){
         std::cout<<"Errore nella generazione dell' aad del pacchetto\n";
         free(iv);
         return;
     }
+
+    //alloco Ciphertext
     unsigned char* ciphertext = (unsigned char*)malloc(SIZE_FILENAME);
     if(!ciphertext){
         std::cout<<"Errore nella malloc\n";
@@ -196,6 +194,8 @@ void send_std_packet(std::string filename, unsigned char* key,int sd, uint64_t* 
         free(aad);
         return;
     }
+
+    //alloco TAG 
     unsigned char* tag = (unsigned char*)malloc(16);
     if(!tag){
         std::cout<<"Errore nella malloc\n";
@@ -206,6 +206,7 @@ void send_std_packet(std::string filename, unsigned char* key,int sd, uint64_t* 
     }
     uint32_t* ciphertext_len = (uint32_t*)malloc(sizeof(uint32_t));
 
+    //cripto la richiesta
     if( !encrypt_gcm((unsigned char*)filename.c_str(),SIZE_FILENAME,aad,STD_AAD_LEN,key,iv,ciphertext,ciphertext_len, tag) ){
         std::cout<<"Errore nella encrypt della richiesta\n";
         free(iv);
@@ -214,6 +215,8 @@ void send_std_packet(std::string filename, unsigned char* key,int sd, uint64_t* 
         free(tag);
         return;
     }
+
+    //costruisco la richiesta
     unsigned char* request = build_request(aad,ciphertext,tag,STD_AAD_LEN,*ciphertext_len);
     if(!request){
         std::cout<<"Errore nella malloc\n";
@@ -223,6 +226,8 @@ void send_std_packet(std::string filename, unsigned char* key,int sd, uint64_t* 
         free(tag);
         return;
     }
+
+    //invio la richiesta
     if(!send_packet<unsigned char>(sd, request, REQ_LEN)){
         std::cout<<"Errore nell'invio della richiesta\n";
         free(iv);
@@ -239,6 +244,8 @@ void send_std_packet(std::string filename, unsigned char* key,int sd, uint64_t* 
     return;
 }
 
+
+/*Metodo che permette di estrapolare i vari parametri della richiesta lato ricevitore*/
 bool read_request_param(unsigned char* request,uint64_t* counter,uint32_t* num_packets, uint8_t* id, unsigned char* plaintext,unsigned char* key){
 
     uint64_t received_count;
@@ -258,17 +265,22 @@ bool read_request_param(unsigned char* request,uint64_t* counter,uint32_t* num_p
     }
     memcpy(ciphertext,request+ STD_AAD_LEN, SIZE_FILENAME);
 
+    //ricavo AAD
     unsigned char* aad = (unsigned char*)malloc(STD_AAD_LEN);
     if(!aad){
         std::cout<<"Errore nella malloc\n";
         return false;
     }
+
+    //ricavo TAG 
     unsigned char* tag = (unsigned char*)malloc(16);
     if(!tag){
         std::cout<<"Errore nella malloc\n";
         free(aad);
         return false;
     }
+
+    //ricavo IV 
     unsigned char* iv = (unsigned char*)malloc(12);
     if(!iv){
         std::cout<<"Errore nella malloc\n";
@@ -281,7 +293,9 @@ bool read_request_param(unsigned char* request,uint64_t* counter,uint32_t* num_p
     memcpy(tag,request + SIZE_FILENAME + STD_AAD_LEN, 16);
     memcpy(aad, request, STD_AAD_LEN);
     uint32_t* plaintext_len = (uint32_t*)malloc(sizeof(uint32_t));
-    if(!decrypt_gcm(ciphertext, SIZE_FILENAME, aad, STD_AAD_LEN, tag, key, iv, plaintext,plaintext_len)){
+
+    //decripto il ciphertext
+    if(!decrypt_gcm(ciphertext, SIZE_FILENAME, aad, STD_AAD_LEN, tag, key, iv , plaintext,plaintext_len)){
         std::cout<<"Errore nella decifratura della richiesta\n";
         free(aad);
         free(tag);
@@ -289,9 +303,10 @@ bool read_request_param(unsigned char* request,uint64_t* counter,uint32_t* num_p
         free(ciphertext);
         return false;
     }   
+
+    //controllo che il counter sia coerente
     if(received_count != *counter){
         std::cout<<"Counter errato\n";
-        std::cout<<received_count<<" != "<<*counter<<std::endl;
         return false;
     }
     (*counter)++;
@@ -302,9 +317,13 @@ bool read_request_param(unsigned char* request,uint64_t* counter,uint32_t* num_p
     return true;
 }
 
+
+
+/*Funzione che costruisce la AAD di un pacchetto data:  
+  1) counter 
+  4) iv */
 unsigned char* build_aad_data(uint64_t counter, unsigned char* iv){
 
-    //aad = counter || iv
     unsigned char* aad = (unsigned char*)malloc(AAD_LEN);
     if(!aad){
         return NULL;
@@ -315,8 +334,11 @@ unsigned char* build_aad_data(uint64_t counter, unsigned char* iv){
     return aad;
 }
 
+
+/*Funzione per inviare un pacchetto data*/
 void send_data_packet(unsigned char* data, unsigned char* key,int sd, uint64_t* counter,uint32_t data_len){
 
+    //genero IV 
     unsigned char* iv = (unsigned char*)malloc(12);
     if(!iv){
         std::cout<<"Errore nella malloc\n";
@@ -324,12 +346,16 @@ void send_data_packet(unsigned char* data, unsigned char* key,int sd, uint64_t* 
     }
     iv = nonce(iv,12);
 
+
+    //creo AAD del pacchetto data  
     unsigned char* aad = build_aad_data(*counter,iv);
     if(!aad){
         std::cout<<"Errore nella generazione dell' aad del pacchetto\n";
         free(iv);
         return ;
     }
+
+    //alloco il ciphertext
     unsigned char* ciphertext = (unsigned char*)malloc(data_len + 16);
     if(!ciphertext){
         std::cout<<"Errore nella malloc\n";
@@ -337,6 +363,8 @@ void send_data_packet(unsigned char* data, unsigned char* key,int sd, uint64_t* 
         free(aad);
         return ;
     }
+
+    //alloco il TAG 
     unsigned char* tag = (unsigned char*)malloc(16);
     if(!tag){
         std::cout<<"Errore nella malloc\n";
@@ -348,6 +376,8 @@ void send_data_packet(unsigned char* data, unsigned char* key,int sd, uint64_t* 
 
     uint32_t* ciphertext_len = (uint32_t*)malloc(sizeof(uint32_t));
     
+
+    //cripto
     if( !encrypt_gcm(data,data_len,aad,AAD_LEN,key,iv,ciphertext,ciphertext_len,tag) ){
         std::cout<<"Errore nella encrypt della richiesta\n";
         free(iv);
@@ -360,6 +390,9 @@ void send_data_packet(unsigned char* data, unsigned char* key,int sd, uint64_t* 
     unsigned char* extended_aad = (unsigned char*)malloc(AAD_LEN + sizeof(uint32_t));
     memcpy(extended_aad, ciphertext_len, sizeof(uint32_t));
     memcpy(extended_aad + sizeof(uint32_t), aad, AAD_LEN);
+
+
+    //costruisco la richiesta 
     unsigned char* request = build_request(extended_aad,ciphertext,tag,AAD_LEN + sizeof(uint32_t),*ciphertext_len);
     if(!request){
         std::cout<<"Errore nella malloc\n";
@@ -370,6 +403,8 @@ void send_data_packet(unsigned char* data, unsigned char* key,int sd, uint64_t* 
         free(extended_aad);
         return ;
     }
+
+    //invio la richiesta
     if(!send_packet<unsigned char>(sd, request, sizeof(uint32_t) + AAD_LEN + *ciphertext_len + 16)){
         std::cout<<"Errore nell'invio della richiesta\n";
         free(iv);
@@ -390,6 +425,9 @@ void send_data_packet(unsigned char* data, unsigned char* key,int sd, uint64_t* 
     return;
 }
 
+
+
+//Funzione che riceve il pacchetto data e ne estrapola i parametri 
 unsigned char* receive_data_packet(int sd,uint64_t* counter,unsigned char* key,uint32_t* plaintext_len){
 
     uint32_t* data_len = recv_packet<uint32_t>(sd,sizeof(uint32_t));
@@ -399,17 +437,25 @@ unsigned char* receive_data_packet(int sd,uint64_t* counter,unsigned char* key,u
 
     uint64_t received_count;
     unsigned char* iv = (unsigned char*)malloc(12);
+
+    //ricavo count
     memcpy(&received_count,aad,sizeof(uint64_t));
+
+
+    //ricavo IV 
     memcpy(iv,aad + sizeof(uint64_t), 12);
 
+
+    //controllo la validità del counter 
     if(received_count != *counter){
         std::cout<<"Errore: counter errato\n";
-        std::cout<<received_count<<" != "<<*counter<<std::endl;
         return NULL;
     }
     (*counter)++;
     unsigned char* plaintext = (unsigned char*)malloc(*data_len);
-    if(!decrypt_gcm(payload, *data_len, aad, AAD_LEN, tag, key, iv, plaintext,plaintext_len)){
+
+    //decripto il ciphertext
+    if(!decrypt_gcm(payload, *data_len, aad, AAD_LEN, tag, key, iv , plaintext, plaintext_len)){
         return NULL;
     }
     free(data_len);
@@ -420,131 +466,8 @@ unsigned char* receive_data_packet(int sd,uint64_t* counter,unsigned char* key,u
     return plaintext;
 }
 
+
+//metodo per dividere un pacchetto di dimensione superiore di MAX_PAYLOAD_SIZE in tanti sottopacchetti di dimensione fissa
 uint32_t how_many_fragments(uint64_t size){
     return size/MAX_PAYLOAD_SIZE + (size % MAX_PAYLOAD_SIZE != 0);
-}
-
-bool write_transfer_op(std::string filename, uint32_t num_packets, int sd, unsigned char* key, uint64_t* counter) {
-    FILE* file = fopen(filename.c_str(),"w+");
-    if(!file) {
-        return false;
-    }
-    unsigned char* plaintext;
-    uint32_t* plaintext_len = (uint32_t*)malloc(sizeof(uint32_t));
-    float progress = 0.0;
-    int barWidth = 70;
-    int pos = barWidth * progress;
-    for(uint32_t i = 0; i < num_packets; i++){
-        plaintext = receive_data_packet(sd,counter,key,plaintext_len);
-        if(!plaintext){
-            std::cout<<"Errore nella ricezione del pacchetto\n";
-            fclose(file);
-            remove(filename.c_str());
-            (*counter) += num_packets - (i+1);
-            clean_socket(sd);
-            return false;
-        }
-        uint32_t ret;
-        ret = fwrite(plaintext,1,*plaintext_len,file);
-        progress = (float)i/num_packets;
-        if(num_packets == 1) {
-            progress = 0.999;
-        }
-
-        std::cout << "[";
-        pos = barWidth * progress;
-        for (int j = 0; j < barWidth; ++j) {
-            if (j < pos) std::cout << "■";
-            else std::cout << " ";
-        }
-        if(i<num_packets-1) {
-            std::cout << "] " << int(progress * 100.0) << " %\r";
-        }
-        else {
-            std::cout << "] " << 100 << " %\r";
-        }
-        std::cout.flush();
-        free(plaintext);
-    }
-    free(plaintext_len);
-    fclose(file);
-    fflush(file);
-    return true;
-}
-
-bool read_transfer_op(std::string username, uint32_t num_packets, uint64_t file_len, std::string filename, int sd, unsigned char* key, uint64_t* counter){
-    FILE* file;
-    if(username.empty()) {
-        file = fopen(filename.c_str(), "r");
-    }
-    else {
-        file = fopen((username + "/" + filename).c_str(),"r");
-    }
-    if(!file) {
-        return false;
-    }
-    unsigned char* data;
-    uint32_t data_len;
-    uint32_t payload_len;
-    float progress = 0.0;
-    int barWidth = 70;
-    int pos = barWidth * progress;
-    for(uint32_t i = 0; i < num_packets; i++){
-        if(num_packets == 1){
-            data = (unsigned char*)malloc(file_len);
-            fread(data,1,file_len,file);
-            data_len = file_len;
-        }
-        else if(num_packets - 1 == i){
-            data = (unsigned char*)malloc(file_len%MAX_PAYLOAD_SIZE);
-            fread(data,1,file_len%MAX_PAYLOAD_SIZE,file);
-            data_len = file_len%MAX_PAYLOAD_SIZE;
-        }
-        else{
-            data = (unsigned char*)malloc(MAX_PAYLOAD_SIZE);
-            fread(data,1,MAX_PAYLOAD_SIZE,file);
-            data_len = MAX_PAYLOAD_SIZE;
-        }
-        send_data_packet(data,key,sd,counter,data_len);
-        progress = (float)i/num_packets;
-        if(num_packets == 1) {
-            progress = 0.999;
-        }
-
-        std::cout << "[";
-        pos = barWidth * progress;
-        for (int j = 0; j < barWidth; ++j) {
-            if (j < pos) std::cout << "■";
-            else std::cout << " ";
-        }
-        if(i<num_packets-1) {
-            std::cout << "] " << int(progress * 100.0) << " %\r";
-        }
-        else {
-            std::cout << "] " << 100 << " %\r";
-        }
-        std::cout.flush();
-        free(data);
-        usleep(1);
-    }
-    fclose(file);
-    return true;
-}
-
-unsigned char* wait_for_done(int sd){
-    
-    fd_set READY;
-    struct timeval* timer = (timeval*)malloc(sizeof(struct timeval));
-    timer->tv_sec = 1;
-    int ret;
-    FD_ZERO(&READY);
-    FD_SET(sd,&READY);
-    ret = select(sd+1,&READY,NULL,NULL,timer);
-    if(!ret){
-        free(timer);
-        return NULL;
-    }
-    
-    free(timer);
-    return recv_packet<unsigned char>(sd,REQ_LEN);
 }
